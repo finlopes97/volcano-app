d7bb84c6c0f1e754149b8500b80b73ab1fe8436f
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-04-22 22:24:24 +1000

    feat(home): add cards with map squares to main page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { Map, Marker } from "pigeon-maps"
 import './style/App.css';
 
+function GetRandomNumber() {
+  return Math.floor(Math.random() * 101);
+}
+
 function App() {
+  const [volcanoes, setVolcanoes] = useState([]);
+
+  useEffect(() => {
+    const fetchVolcanoData = async () => {
+      const responses = await Promise.all([
+        fetchVolcano(GetRandomNumber()),
+        fetchVolcano(GetRandomNumber()),
+        fetchVolcano(GetRandomNumber()),
+        fetchVolcano(GetRandomNumber())
+      ]);
+
+      const data = await Promise.all(responses.map(response => response.json()));
+      setVolcanoes(data);
+    };
+
+    fetchVolcanoData();
+  }, []);
+
+  const fetchVolcano = (volcanoId) => {
+    return fetch(`http://4.237.58.241:3000/volcano/${volcanoId}`);
+  }
+
   return (
       <div className="App">
         <header className="App-header">
@@ -36,36 +63,34 @@ function App() {
               </form>
             </div>
           </div>
-          <div class="container row quote parallax">
+          <div className="container row quote parallax">
             <blockquote cite="Meet Dave">    
               This is a picture of a volcano with a sick parallax effect, if only there were more stuff
               on this page I could scroll down.
-              <div class="cite">- Whimsical Web Developer</div>
+              <div className="cite">- Whimsical Web Developer</div>
             </blockquote>
           </div>
-            <div class="container col content">
-              <h3>Some random information.</h3>
-                <div class="container row">
-                  <div class="card">
-                    <h3>Placeholder</h3>
-                    <p>this is some subtext under an illustration or image</p>
-                  </div>
-                  <div class="card">
-                    <h3>Placeholder</h3>
-                    <p>this is some subtext under an illustration or image</p>
-                  </div>
-                  <div class="card">
-                    <h3>Placeholder</h3>
-                    <p>this is some subtext under an illustration or image</p>
-                  </div>
-                  <div class="card">
-                    <h3>Placeholder</h3>
-                    <p>this is some subtext under an illustration or image</p>
+          <div className="container col content">
+            <h3>Volcanoes of the Week</h3>
+            <div className="container row">
+              {volcanoes.map((volcano, index) => (
+                <div key={index}>
+                  <div className="card">
+                    <h3 className="card-title">{volcano.name}</h3>
+                    <Map height={200} defaultCenter={[parseFloat(volcano.latitude), parseFloat(volcano.longitude)]} defaultZoom={11}>
+                      <Marker width={1} anchor={[parseFloat(volcano.latitude), parseFloat(volcano.longitude)]} />
+                    </Map>
+                    <p>Country: {volcano.country}</p>
+                    <p>Region: {volcano.region}</p>
+                    <p>Summit: {volcano.summit}</p>
+                    <p>Last Eruption: {volcano.last_eruption}</p>
                   </div>
                 </div>
+              ))}
             </div>
+        </div>
         </main>
-        <footer class="container row footer">
+        <footer className="container row footer">
           © 1997-2024 FINLOPES97 
         </footer>
       </div>
