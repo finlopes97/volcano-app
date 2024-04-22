3596d6a1243493788fa6436839be4186fb35521f
finlopes97 <Kyle.Finlayson1997@gmail.com>
2024-04-22 21:19:18 +1000

    oops(lots): forget to make small commits I can't keep getting away with this!

--- src/App.js
+++ src/App.js
@@ -1,39 +1,73 @@
 import React, { useState } from 'react';
 import './style/App.css';
 
-function fetchData(callback) {
-  fetch('http://4.237.58.241:3000/volcanoes?country=Algeria&populatedWithin=100km')
-    .then(response => response.json())
-    .then(data => callback(data))
-    .catch(error => console.error('Error fetching data:', error));
-}
-
 function App() {
-  const [colorBlindMode, setColorBlindMode] = useState('normal');
-
-  const toggleColorBlindMode = (mode) => {
-    setColorBlindMode(mode);
-  }
-
-  const handleFetchData = () => {
-    fetchData(response => {
-      console.log(response);
-    });
-  };
-
   return (
-      <div className="App colorBlindMode">
+      <div className="App">
         <header className="App-header">
-            <h1 className="title">Hot Volcanoes in Your Area</h1>
             <nav>
-                <ul className="navList">
-                    <button className="nav-button colorBlindMode">Home</button>
-                    <button className="nav-button colorBlindMode">Volcano List</button>
-                    <button className="nav-button colorBlindMode">Sign Up</button>
-                    <button className="nav-button colorBlindMode">Login</button>
+                <ul className="nav-list">
+                    <li id="first-link"><a className="nav-link"><h1>Home</h1></a></li>
+                    <li><a className="nav-link">Volcano List</a></li>
+                    <li><a className="nav-link">Sign Up</a></li>
+                    <li><a className="nav-link">Login</a></li>
                 </ul>
             </nav>
         </header>
+        <main>
+          <div className="container row jumbotron">
+            <div className="container col">
+              <h2>Hot Volcanoes In Your Area</h2>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Tincidunt eget nullam non nisi. 
+                Sit amet facilisis magna etiam tempor orci. Turpis cursus in hac habitasse. 
+                Diam maecenas ultricies mi eget mauris pharetra. Velit euismod in pellentesque 
+                massa placerat duis ultricies. Vitae turpis massa sed elementum tempus egestas 
+                sed sed risus.
+              </p>
+              <h3 id="cta-heading">Stay Connected</h3>
+              <form id="cta-form" className="container row">
+                <label className="container col">
+                  Your Email:
+                  <input type="email" name="userEmail" />
+                </label>
+                <input id="cta-submit" type="submit" value="Sign Up" />
+              </form>
+            </div>
+          </div>
+          <div class="container row quote parallax">
+            <blockquote cite="Meet Dave">    
+              This is a picture of a volcano with a sick parallax effect, if only there were more stuff
+              on this page I could scroll down.
+              <div class="cite">- Whimsical Web Developer</div>
+            </blockquote>
+          </div>
+            <div class="container col content">
+              <h3>Some random information.</h3>
+                <div class="container row">
+                  <div class="card">
+                    <h3>Placeholder</h3>
+                    <p>this is some subtext under an illustration or image</p>
+                  </div>
+                  <div class="card">
+                    <h3>Placeholder</h3>
+                    <p>this is some subtext under an illustration or image</p>
+                  </div>
+                  <div class="card">
+                    <h3>Placeholder</h3>
+                    <p>this is some subtext under an illustration or image</p>
+                  </div>
+                  <div class="card">
+                    <h3>Placeholder</h3>
+                    <p>this is some subtext under an illustration or image</p>
+                  </div>
+                </div>
+            </div>
+        </main>
+        <footer class="container row footer">
+          © 1997-2024 FINLOPES97 
+        </footer>
       </div>
   );
 }
