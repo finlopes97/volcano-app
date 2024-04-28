3f10eb6a67f27ff71809b926e38d8642e4f407e4
finlopes97 <kyle.finlayson1997@gmail.com>
2024-04-29 09:03:41 +1000

    Adding auth, halfway there

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -12,6 +12,7 @@ function SignUp() {
   const email = queryParams.get("email");
   const [showPassword, setShowPassword] = useState(true);
   const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
   const handleGoBack = () => {
     navigate(`/`);
@@ -29,9 +30,58 @@ function SignUp() {
     setShowPassword(!showPassword);
   };
 
+  const handleSubmit = (e) => {
+	e.preventDefault();
+
+	const userData = {
+		email: document.getElementById("signup-email").value,
+		password: document.getElementById("signup-password").value
+	};
+
+	const userDataJson = JSON.stringify(userData);
+
+	fetch("http://4.237.58.241:3000/user/register", {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		body: userDataJson
+	})
+	.then(response => response.json())
+	.then(data => {
+		if(data.message === "User created") {
+			// Automatically log this bitch in
+			return fetch("http://4.237.58.241:3000/user/login", {
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					'Accept': 'application/json'
+				},
+				body: userDataJson
+			});
+		} else {
+			throw new Error(`Failed to create user: ${data.message}`);
+		}
+	})
+	.then(response => response.json())
+	.then(data => {
+		if(data.token) {
+			localStorage.setItem("authToken", data.token);
+			navigate('/');
+		} else {
+			throw new Error("Failed to login after registration. Try again.")
+		}
+	})
+	.catch(error => {
+		console.error(`Error: ${error}`);
+		setError(`Error: ${error.message}`);
+	});
+  };
+
   return (
     <div className="container col signup-form">
-      <form className="container col">
+      <form className="container col" onSubmit={handleSubmit}>
         <h2 className="signup-heading">Create a free account.</h2>
         <label className="container col">
           Email:
